c8da69ca40c7b6e3d3e71708e6c784f34deb7866
jiyeon lee <a1919361@gmail.com>
2020-12-11 23:05:50 +0900

    콤보, 승패표시 추가

--- rock-scissors-paper/README.md
+++ rock-scissors-paper/README.md
@@ -18,3 +18,9 @@ const found = Object.entries(options).findIndex((v) => {
 
 ## 20201211
 - (a === x || b === x) 처럼 or 관계일 때 includes로 코드 줄일 수 있다
+
+- [ ] badge, combo와 관련된 변수와 함수 모아서 객체로 만들어보기
+- [x] 연승 표시
+- [x] 승, 패 표시
+- [x] 내가 누른 버튼 표시
+- [x] 버튼 눌렀을 때 플래그 변수 바꿔서 setInterval중복실행 방지
--- rock-scissors-paper/script.js
+++ rock-scissors-paper/script.js
@@ -1,10 +1,22 @@
+const SELECTED_CLASS = "selected";
+const SHOW_CLASS = "show";
+
+let interval;
 let imageCoord = 0;
 
+let comboCount = 0;
+
+let playing = false;
 let options = {
   scissor: 0,
   rock: -300,
   paper: -600,
 };
+
+const computerContainer = document.querySelector("#computer");
+const resultBadge = document.querySelector(".result.badge");
+const combo = document.querySelector(".combo");
+const comboText = combo.querySelector("span");
 
 function getComputerSelection(imageCoord) {
   return Object.entries(options).find((v) => {
@@ -12,7 +24,11 @@ function getComputerSelection(imageCoord) {
   })[0];
 }
 
-let interval;
+function startPlaying() {
+  playing = true;
+  resultBadge.classList.remove(SHOW_CLASS);
+  intervalMaker();
+}
 
 function intervalMaker() {
   interval = setInterval(() => {
@@ -23,28 +39,38 @@ function intervalMaker() {
     } else {
       imageCoord = options.rock;
     }
-    document.querySelector(
-      "#computer"
-    ).style.background = `url(image.jpg) ${imageCoord}px 0`;
+    computerContainer.style.background = `url(image.jpg) ${imageCoord}px 0`;
   }, 100);
 }
 
-intervalMaker();
+combo.addEventListener("animationend", (e) => {
+  console.log("animation end");
+  combo.classList.remove("animate");
+});
 
 document.querySelectorAll(".btn").forEach((btn) => {
   btn.addEventListener("click", function () {
+    if (!playing) return;
+    playing = false;
+
+    let prevSelectedBtn = this.parentElement.querySelector(
+      `.${SELECTED_CLASS}`
+    );
+    if (prevSelectedBtn) {
+      prevSelectedBtn.classList.remove(SELECTED_CLASS);
+    }
+    this.classList.add(SELECTED_CLASS);
+
+    combo.classList.remove("animate");
+
     clearInterval(interval);
+
     setTimeout(() => {
-      intervalMaker();
+      startPlaying();
     }, 1000);
-    let koDict = {
-      가위: "scissor",
-      바위: "rock",
-      보: "paper",
-    };
-    let mySelection = koDict[this.textContent];
+
+    let mySelection = this.id;
     let computerSelection = getComputerSelection(imageCoord);
-    console.log(mySelection, computerSelection);
 
     const score = {
       scissor: 0,
@@ -53,12 +79,32 @@ document.querySelectorAll(".btn").forEach((btn) => {
     };
 
     let scoreDiff = score[mySelection] - score[computerSelection];
+
     if (scoreDiff === 0) {
-      console.log("비겼습니다");
+      resultBadge.textContent = "다시";
+      resultBadge.style.backgroundColor = "turquoise";
     } else if ([-1, 2].includes(scoreDiff)) {
-      console.log("졌습니다");
+      resultBadge.textContent = "패";
+      resultBadge.style.backgroundColor = "red";
+      comboCount = 0;
     } else {
-      console.log("이겼습니다");
+      resultBadge.textContent = "승";
+      resultBadge.style.backgroundColor = "blue";
+      comboCount += 1;
+      combo.classList.add("animate");
     }
+
+    comboText.textContent = comboCount;
+
+    if (comboCount) {
+      combo.classList.add(SHOW_CLASS);
+    } else {
+      combo.classList.remove(SHOW_CLASS);
+    }
+
+    resultBadge.classList.add(SHOW_CLASS);
+    resultBadge.style.left = `${this.offsetLeft - 16}px`; // badge의 width 1/3만큼 이동
   });
 });
+
+startPlaying();
